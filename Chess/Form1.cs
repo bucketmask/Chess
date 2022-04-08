7d017db0f555f580d5b78c2c5cc77091e190638b
liam_port <liamp@DESKTOP-ABPD3AP>
2022-04-08 11:55:19 +0100

    done, can show board from fen with picturebox in class[]

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -21,8 +21,6 @@ namespace Chess
 
         public void DrawChessGUI()
         {
-            //creates the chess board class
-            board board = new board();
 
 
             //form
@@ -43,65 +41,9 @@ namespace Chess
             panel1.BackColor = System.Drawing.Color.Blue;
 
 
-            ////makes grid
-            //TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
-            //tableLayoutPanel1.RowCount = 8;
-            //tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1/8));
-            //tableLayoutPanel1.ColumnCount = 8;
-            //tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1 / 8));
-            //tableLayoutPanel1.Dock = DockStyle.Fill;
-            //tableLayoutPanel1.BackColor = Color.Blue;
-            //tableLayoutPanel1.Margin = new Padding(0);
-            //tableLayoutPanel1.Padding = new Padding(0);
+            //creates the chess board class
+            board board = new board(panel1);
 
-
-            //tableLayoutPanel1.Click += new EventHandler(board.userclick);
-
-            //panel1.Controls.Add(tableLayoutPanel1);
-
-
-
-
-            //chess squares as buttons with positional info
-
-
-            for (int x = 0; x < 8; x++)
-            {
-                for(int y = 0; y < 8; y++)
-                {
-                    //Console.WriteLine($"({x}:{y}){coord[0]}:{coord[1]}");
-
-
-                    PictureBox pictureBox1 = new PictureBox();
-                    pictureBox1.Padding = new Padding(0, 0, 0, 0);
-                    pictureBox1.Margin = new Padding(0);
-                    pictureBox1.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8);
-                    pictureBox1.Location = new Point(x * panel1.Size.Width / 8, y * panel1.Size.Height / 8);
-
-                    if ((x % 2 == 0 && y % 2 == 1) || (x % 2 == 1 && y % 2 == 0))
-                    {
-                        pictureBox1.BackColor = Color.Black;
-                        pictureBox1.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8);;
-
-                    }
-                    else 
-                    {
-                        pictureBox1.BackColor= Color.White;
-                        pictureBox1.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8);
-                    }
-
-                    //this will execute a function when the botton is pressed
-                    //button.Click += new  EventHandler(board.userclick);
-                    pictureBox1.Click += new EventHandler(board.userclick);
-                    panel1.Controls.Add((pictureBox1));
-                }
-            }
-
-
-
-
-            //draws the chess board on
-            board.DrawBoard(panel1);
 
 
 
--- Chess/board.cs
+++ Chess/board.cs
@@ -1,34 +1,143 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Chess
 {
     public class board
     {
-        pieces[] boardArr = new pieces[64];
-        public board()
+        PictureBox[] ParentSquares = new PictureBox[64];
+
+
+
+        public board(Panel panel1)
         {
-            boardArr[1] = new pawn(1,0);
-            boardArr[63] = new pawn(7,7);
-            
+            pieces[] boardpieces = CreateBoardFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    PictureBox pictureBox1 = new PictureBox();
+                    ParentSquares[y * 8 + x] = pictureBox1;
+                    pictureBox1.Padding = new Padding(0, 0, 0, 0);
+                    pictureBox1.Margin = new Padding(0);
+                    pictureBox1.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8);
+                    pictureBox1.Location = new Point(x * panel1.Size.Width / 8, y * panel1.Size.Height / 8);
+
+                    if ((x % 2 == 0 && y % 2 == 1) || (x % 2 == 1 && y % 2 == 0))
+                    {
+                        pictureBox1.BackColor = Color.Brown;
+                        pictureBox1.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8); ;
+
+                    }
+                    else
+                    {
+                        pictureBox1.BackColor = Color.White;
+                        pictureBox1.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8);
+                    }
+                    panel1.Controls.Add((pictureBox1));
+
+                }
+            }
+
+            //test
+            DrawPieces(panel1, boardpieces);
+            //test
+
         }
 
-        public void DrawBoard(System.Windows.Forms.Panel tableLayoutPanel1)
+        public void DrawPieces(Panel panel1, pieces[] boardpieces)
         {
-            Console.WriteLine(tableLayoutPanel1.Location);
-            //every obj is drawn
-            for (int i = 0; i < 64; i++)
+            for (int i = 0; i < boardpieces.Length; i++)
             {
-                if (boardArr[i] != null) { boardArr[i].draw(tableLayoutPanel1); }
+                if (boardpieces[i] != null) 
+                {
+                    Console.WriteLine(boardpieces[i]);
+                    boardpieces[i].pictureBox.Location = new Point(i % 8 * panel1.Size.Width/8, i / 8 * panel1.Size.Width / 8);
+                    boardpieces[i].pictureBox.Size = new Size(panel1.Size.Width / 8, panel1.Size.Height / 8);
+                    boardpieces[i].pictureBox.BackColor = ParentSquares[i].BackColor;
+                    panel1.Controls.Add(boardpieces[i].pictureBox);
+                    boardpieces[i].pictureBox.BringToFront();
+                    Console.WriteLine(boardpieces[i].pictureBox.Size);
+                    Console.WriteLine($"{boardpieces[i].pictureBox.Location}:{ParentSquares[i].Location}");
+                    Console.WriteLine(boardpieces[i].pictureBox.Parent);
+                }
+                
             }
         }
-        public void userclick(object sender, EventArgs e)
+
+        public pieces[] CreateBoardFromFen(string fen)
         {
-            System.Windows.Forms.PictureBox picture = sender as System.Windows.Forms.PictureBox;
-            Console.WriteLine(picture.Location);
+            pieces[] boardpieces = new pieces[64];
+
+            string[] fenSplit = fen.Split('/');
+
+            int Pointer = 0;
+            for (int i = 0; i < fenSplit.Length; i++)
+            {
+                for (int j = 0; j < fenSplit[i].Length; j++)
+                {
+                    int number;
+                    if (int.TryParse(fenSplit[i][j].ToString(), out number))
+                    {
+                        Pointer += number - 1;
+                    }
+                        if (fenSplit[i][j].ToString().ToLower() == "p")
+                    {
+                        if (char.IsLower(fenSplit[i][j])) { boardpieces[Pointer] = new pawn(false); }
+                        else if (char.IsUpper(fenSplit[i][j])) { boardpieces[Pointer] = new pawn(true); }
+
+                    }
+
+                    else if (fenSplit[i][j].ToString().ToLower() == "r")
+                    {
+                        if (char.IsLower(fenSplit[i][j])) { boardpieces[Pointer] = new rook(false); }
+                        else if (char.IsUpper(fenSplit[i][j])) { boardpieces[Pointer] = new rook(true); }
+                    }
+                    else if (fenSplit[i][j].ToString().ToLower() == "b")
+                    {
+                        if (char.IsLower(fenSplit[i][j])) { boardpieces[Pointer] = new bishop(false); }
+                        else if (char.IsUpper(fenSplit[i][j])) { boardpieces[Pointer] = new bishop(true); }
+                    }
+                    else if (fenSplit[i][j].ToString().ToLower() == "n")
+                    {
+                        if (char.IsLower(fenSplit[i][j])) { boardpieces[Pointer] = new knight(false); }
+                        else if (char.IsUpper(fenSplit[i][j])) { boardpieces[Pointer] = new knight(true); }
+                    }
+                    else if (fenSplit[i][j].ToString().ToLower() == "k")
+                    {
+                        if (char.IsLower(fenSplit[i][j])) { boardpieces[Pointer] = new king(false); }
+                        else if (char.IsUpper(fenSplit[i][j])) { boardpieces[Pointer] = new king(true); }
+                    }
+                    else if (fenSplit[i][j].ToString().ToLower() == "q")
+                    {
+                        if (char.IsLower(fenSplit[i][j])) { boardpieces[Pointer] = new queen(false); }
+                        else if (char.IsUpper(fenSplit[i][j])) { boardpieces[Pointer] = new queen(true); }
+                    }
+                    Pointer++;
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return boardpieces;
         }
+
+
     }
 }
